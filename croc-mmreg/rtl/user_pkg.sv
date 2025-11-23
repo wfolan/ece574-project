8619a44acda8ab5a44d36e714e4bfcef939ee788
ljdukette <ljdukette@wpi.edu>
2025-11-23 14:17:40 -0500

    Adding the other two

--- croc-mmreg/rtl/user_pkg.sv
+++ croc-mmreg/rtl/user_pkg.sv
@@ -9,47 +9,33 @@
 `include "obi/typedef.svh"
 
 package user_pkg;
-   
-   ////////////////////////////////
-   // User Manager Address maps //
-   ///////////////////////////////
-   
-   // None
-   
-   
-   /////////////////////////////////////
-   // User Subordinate Address maps ////
-   /////////////////////////////////////
-   
-   localparam int unsigned NumUserDomainSubordinates = 3; // ROM + MMREG PLUS CORDIC
-   
-   localparam bit [31:0]   UserRomAddrOffset   = croc_pkg::UserBaseAddr; // 32'h2000_0000;
-   localparam bit [31:0]   UserRomAddrRange    = 32'h0000_1000;          // every subordinate has at least 4KB
-   
-   localparam bit [31:0]   UserMmregAddrOffset = UserRomAddrOffset + UserRomAddrRange; // 32'h2000_1000
-   localparam bit [31:0]   UserMmregAddrRange  = 32'h0000_0020;
-   
-   //ADDITION::: CORDIC COPROCESSOR
-   localparam bit [31:0] UserCordicAddrOffset = UserMmregAddrOffset + UserMmregAddrRange; // 0x2000_1020
-   localparam bit [31:0] UserCordicAddrRange  = 32'h0000_0040;          // 64 B for 4 regs
 
-   localparam int unsigned NumDemuxSbrRules  = NumUserDomainSubordinates; // number of address rules in the decoder
-   localparam int unsigned NumDemuxSbr       = NumDemuxSbrRules + 1; // additional OBI error, used for signal arrays
-   
-   // Enum for bus indices
-   typedef enum int {
-		     UserError = 0,
-		     UserRom = 1,
-		     UserMmreg = 2,
-           UserCordic = 3 //ADDITION
-		     } user_demux_outputs_e;
-   
+  ////////////////////////////////
+  // User Manager Address maps //
+  ///////////////////////////////
+  
+  // None
+
+
+  /////////////////////////////////////
+  // User Subordinate Address maps ////
+  /////////////////////////////////////
+
+  localparam int unsigned NumUserDomainSubordinates = 1;
+
+  localparam bit [31:0] UserRomAddrOffset   = croc_pkg::UserBaseAddr; // 32'h2000_0000;
+  localparam bit [31:0] UserRomAddrRange    = 32'h0000_1000;          // every subordinate has at least 4KB
+
+  localparam int unsigned NumDemuxSbrRules  = NumUserDomainSubordinates; // number of address rules in the decoder
+  localparam int unsigned NumDemuxSbr       = NumDemuxSbrRules + 1; // additional OBI error, used for signal arrays
+
+  // Enum for bus indices
+  typedef enum int {
+    UserError = 0
+    UserTinyCache = 1   // tiny cache subordinate
+  } user_demux_outputs_e;
+
   // Address rules given to address decoder
-   localparam	croc_pkg::addr_map_rule_t [NumDemuxSbrRules-1:0] 
-		user_addr_map = '{   
-				     '{ idx: UserRom,    start_addr: UserRomAddrOffset,    end_addr: UserRomAddrOffset  + UserRomAddrRange  },     
-				     '{ idx: UserMmreg,  start_addr: UserMmregAddrOffset,  end_addr: UserMmregAddrOffset + UserMmregAddrRange },    
-				     '{ idx: UserCordic,  start_addr: UserCordicAddrOffset,  end_addr: UserCordicAddrOffset + UserCordicAddrRange }   														   
-      };
-   
+  localparam croc_pkg::addr_map_rule_t [NumDemuxSbrRules-1:0] user_addr_map = '0;
+
 endpackage
